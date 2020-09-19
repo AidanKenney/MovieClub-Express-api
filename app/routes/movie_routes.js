be72b7b8d53e14136b2a9fe97950149b39ea1df6
AidanKenney <kenneyaidan@gmail.com>
2020-09-19 13:02:09 -0400

    Create working update/destroy paths for movies

--- app/routes/movie_routes.js
+++ app/routes/movie_routes.js
@@ -18,7 +18,7 @@ const handle404 = customErrors.handle404
 // that's owned by someone else
 const requireOwnership = customErrors.requireOwnership
 
-const requireOwnershipBool = customErrors.requireOwnershipBool
+// const requireOwnershipBool = customErrors.requireOwnershipBool
 
 // this is middleware that will remove blank fields from `req.body`, e.g.
 // { example: { title: '', text: 'foo' } } -> { example: { text: 'foo' } }
@@ -32,6 +32,7 @@ const requireToken = passport.authenticate('bearer', { session: false })
 // instantiate a router (mini app that only handles routes)
 const router = express.Router()
 
+// CREATE working
 router.post('/movies', requireToken, (req, res, next) => {
   // store incoming request's movie data
   const movieData = req.body.movie
@@ -46,6 +47,55 @@ router.post('/movies', requireToken, (req, res, next) => {
       return collection.save()
     })
     .then(collection => res.status(201).json({ collection }))
+    .catch(next)
+})
+
+// GET all
+// router.get('/movies', requireToken, (req, res, next) => {
+//   let arrayOfOwnersMovies = []
+//   Movie.find()
+//     .then(movies)
+// })
+
+// UPDATE working
+router.patch('/movies/:id', requireToken, removeBlanks, (req, res, next) => {
+  // save movie info from request
+  const movieData = req.body.movie
+  // save movie id indicated by request
+  const movieId = req.params.id
+  // find a collection with a movie that matches movieId
+  Collection.findOne({ 'movies._id': movieId })
+    .then(handle404)
+    .then(collection => {
+      // make sure user owns movie they want to update
+      requireOwnership(req, collection)
+      return collection
+    })
+    .then(collection => {
+      const movie = collection.movies.id(movieId)
+      // replace movie's data with movieData
+      movie.set(movieData)
+      return collection.save()
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+router.delete('/movies/:id', requireToken, (req, res, next) => {
+  const movieId = req.params.id
+  Collection.findOne({ 'movies._id': movieId })
+    .then(handle404)
+    .then(collection => {
+      requireOwnership(req, collection)
+      return collection
+    })
+    .then(collection => {
+      const movie = collection.movies.id(movieId)
+      movie.remove()
+      return collection.save()
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
 })
 
 module.exports = router
---
+++ curl-scripts/movies/update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/movies"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+--header "Authorization: Bearer ${TOKEN}" \
+--data '{
+    "movie": {
+      "title": "'"${TITLE}"'",
+      "releaseDate":  "'"${RELEASEDATE}"'",
+      "description": "'"${DESCRIPTION}"'"
+    }
+  }'
+
+echo
